a4034f6b7e726e1bddf50395e2793b492ca6ef7f
Mac Howe <mac@machowe.com>
2025-12-02 19:13:33 -0500

    test by default

--- boilerplate.py
+++ boilerplate.py
@@ -2,13 +2,13 @@ from aoc2025.utils.utils import read_input
 
 
 def star1() -> int:
-    data = read_input()
+    data = read_input(test=True)
     # TODO: Implement star 1 solution
     return 0
 
 
 def star2() -> int:
-    data = read_input()
+    data = read_input(test=True)
     # TODO: Implement star 2 solution
     return 0
 
